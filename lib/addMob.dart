28da692d56c400de9a4d0ad673b5e1b8747183b7
Sourav <souravcse94@gmail.com>
2019-12-18 02:02:51 +0600

    Update addMob.dart

--- lib/addMob.dart
+++ lib/addMob.dart
@@ -2,18 +2,18 @@ import 'dart:io';
 
 String getAppId() {
  if (Platform.isIOS) {
-   return 'ca-app-pub-3281095153278672~8502044426';
+   return '';
  } else if (Platform.isAndroid) {
-   return 'ca-app-pub-3281095153278672~5545257332';
+   return '';
  }
  return null;
 }
 
 String getAdUnitId() {
   if (Platform.isIOS) {
-    return 'ca-app-pub-3281095153278672/8314342168';
+    return '';
   } else if (Platform.isAndroid) {
-    return 'ca-app-pub-3281095153278672/6460552057';
+    return '';
   }
   return null;
 }
